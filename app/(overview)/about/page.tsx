7e1095a546b50008164f82cce3b40f62cd8bc371
DCerm <138310097+DCerm@users.noreply.github.com>
2025-09-21 19:45:49 +0100

    removed fn content in about page

---
+++ app/(overview)/about/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import { Slide } from "../ui/animations";
+import { H1, H2, H4 } from "../ui/headings";
+import { RegistrationForm } from "../ui/forms";
+import { FaCertificate, FaChartLine, FaHandshake, FaLeaf } from 'react-icons/fa';
+
+
+export default function Page() {
+    return (
+        <>
+       
+
+        </>
+    )
+}
